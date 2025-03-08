38a80fc605ec48c37b55f8e79a2f8ca7da9be259
BrokenTime9 <cerberuscraniax@gmail.com>
2025-03-08 10:27:12 +0530

    hero 1 changes

--- src/components/hero1/index.tsx
+++ src/components/hero1/index.tsx
@@ -5,20 +5,20 @@ import hero1 from "@/../public/hero1.jpg";
 const Hero1 = () => {
   return (
     <div className="flex flex-col text-center p-2 mt-16 gap-4">
-      <h1 className="text-6xl font-semibold bg-gradient-to-r from-[#4b00c8] via-[#6508ba] to-[#d50be4] bg-clip-text text-transparent inline-block p-2">
+      <h1 className="text-3xl font-semibold bg-gradient-to-r from-[#4b00c8] via-[#6508ba] to-[#d50be4] bg-clip-text text-transparent inline-block">
         Master the Art of Digital Illustration
       </h1>
-      <h2 className="text-4xl font-semibold text-gray-600">
+      <h2 className="text-2xl font-semibold text-gray-600">
         A 4-Week Course Designed for Beginners & Enthusiasts!
       </h2>
-      <div className="w-[100%] flex justify-center py-12 m-0">
+      <div className="w-[100%] flex justify-center py-8 m-0">
         <Image src={hero1} alt="art image" />
       </div>
-      <p className="text-3xl font-semibold">
+      <p className="text-xl font-semibold">
         Learn to create stunning digital artwork with step-by-step guidance from
         industry experts.{" "}
       </p>
-      <p className="text-3xl font-semibold">
+      <p className="text-xl font-semibold">
         No prior experience required. Just your creativity! Enroll now for
         ₹7,999
       </p>
