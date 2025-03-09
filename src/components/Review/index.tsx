9af38c6f628d7196f84ed6c484a4c561ddb284e1
BrokenTime9 <cerberuscraniax@gmail.com>
2025-03-09 12:52:36 +0530

    gradient change, padding addition -error removal

--- src/components/Review/index.tsx
+++ src/components/Review/index.tsx
@@ -11,14 +11,12 @@ const Review = () => {
       <div className="flex flex-col">
         <ReviewBox
           name={"Aditi Verma"}
-          age={21}
           review={
             "I always wanted to learn digital art, but tutorials were confusing. This course made everything so simple! Now, I m taking commissions online!"
           }
         />
         <ReviewBox
           name={"Rajesh Khanna"}
-          age={55}
           review={
             "Switching from traditional art to digital felt overwhelming, but this course changed everything! The structured lessons helped me master Procreate in weeks!"
           }
